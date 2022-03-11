d651a6b4a42a2545a949336c63a5e622fc0ec23b
pascal2203 <fluen2203@gmail.com>
2022-03-11 10:31:40 +0100

    Prototype basicly done

--- Script.js
+++ Script.js
@@ -1,1 +1,15 @@
-Hej
+function Choice1 () {
+    ChangeImage('TestBilled','BuzzLightYearBanging.jpg')
+    alert("Lidt gay du gerne vil kneppe ham")
+}
+
+function Choice2 () {
+    ChangeImage('TestBilled','WoodyFalling.jpg')
+    alert("Kæft du grim for at være team buzz lightyear")
+}
+
+function Choice3 () {
+    ChangeImage('TestBilled','BuzzStealingWallet.jpg')
+    alert("The right choise")
+}
+
