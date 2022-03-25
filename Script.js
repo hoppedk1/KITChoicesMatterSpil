abb4c30bddc48f91d45d21dfb86e73437c25cb56
pascal2203 <fluen2203@gmail.com>
2022-03-25 09:44:16 +0100

    Prototype woody version

--- Script.js
+++ Script.js
@@ -1,14 +1,14 @@
 //Game variables
 
-function Choice1 (ScenePicture) {
-    ChangeImage('TestBilled',ScenePicture)
+function Choice1 () {
+    ChangeImage("TestBilled",'BuzzLightYearBanging.jpg')
 }
 
-function Choice2 (ScenePicture) {
-    ChangeImage('TestBilled',ScenePicture)
+function Choice2 () {
+    ChangeImage("TestBilled",'WoodyFalling.jpg')
 }
 
-function Choice3 (ScenePicture) {
-    ChangeImage('TestBilled',ScenePicture)
+function Choice3 () {
+    ChangeImage("TestBilled",'BuzzStealingWallet.jpg')
 }
 
